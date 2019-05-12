9ff9b4b74805d91daaf71eb085ba5e41ebd0f558
Sai Reshmanjali G <reshmanjali.g@gmail.com>
2019-05-12 18:07:23 +0530

    Single inheritance
    A simple java program that demonstrates single inheritance

--- InheritDemo.java
+++ InheritDemo.java
@@ -1,17 +1,18 @@
 class Base{
-	static int i;
+	static int i;		//making an integer static, assigns its value to 0
 	Base(){
-		i++;		//1
+		i++;		//i = 1. Incrementing from 0
 		System.out.println("Base constructor. i = "+i);
 	}
 }
 class InheritDemo extends Base{
 	InheritDemo(){
-		i++;		//2
+		i++;		//i = 2. InheritDemo is the derived class of "Base" class and so "i" is accessible from here
 		System.out.println("Derived constructor. i = "+i);
 	}
 	public static void main(String args[]){
-		new InheritDemo();
-		System.out.println("Main method. i= "+i);		//2
+		new InheritDemo();		//Creating an onject for the derived class fires the Base class constructor earlier to firing the derived class constructor.(Run and see the output:))
+		System.out.println("Main method. i= "+i);	//i = 2. Since i is not declared as private, it is accessible from outside the class 
+		
 	}
 }
