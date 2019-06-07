8d71a3edc15149bc746539605bc650fa5d3c082f
Sai Reshmanjali G <reshmanjali.g@gmail.com>
2019-06-07 13:28:07 +0530

    Update StairCaseJump.java

--- StairCaseJump.java
+++ StairCaseJump.java
@@ -1,4 +1,5 @@
-//There are n stairs, a person standing at the bottom wants to reach the top. The person can climb either 1 stair or 2 stairs at a time. Count the number of ways, the person can reach the top.
+//There are n stairs, a person standing at the bottom wants to reach the top. The person can climb either 1 stair or 2 stairs at a time. 
+//Count the number of ways, the person can reach the top.
 
 import java.util.*;
 class StairCaseJump{
