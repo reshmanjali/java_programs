e7c268cf5bb548da0993f201bf3734c79d7135b4
Sai Reshmanjali G <reshmanjali.g@gmail.com>
2022-09-08 01:37:57 -0400

    Java Primality Test
    A prime number is a natural number greater than 1 whose only positive divisors are 1 and itself. For example, the first six prime numbers are 2, 3, 5, 7, 11, and 13.
    
    Given a large integer, n, use the Java BigInteger class' isProbablePrime method to determine and print whether it's prime or not prime.

---
+++ JavaPrimalityTest.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String n = bufferedReader.readLine();
+        bufferedReader.close();
+    
+        BigInteger b = new BigInteger(n);
+        if(b.isProbablePrime(1)){
+            System.out.print("prime");
+        }
+        else{
+            System.out.print("not prime");
+        }
+    }
+}
